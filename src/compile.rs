d1caf662a526e3e0a30770bcbad5e2f4e9913f9c
Eli Kim <me@eunchan.kim>
2024-02-07 19:07:49 -0800

    Add MoveUp Transform function.

--- src/compile.rs
+++ src/compile.rs
@@ -15,7 +15,7 @@ use asset::Asset;
 
 use comrak::{Arena, parse_document, format_html, Options};
 use comrak::nodes::{AstNode, NodeValue};
-use std::path::{Path, PathBuf};
+use std::path::{Path, PathBuf, Components};
 
 // TODO: Add context, config, other manipulators.
 //
@@ -39,19 +39,42 @@ pub fn compile_page(p: &mut Page, transform: &dyn Fn(&PathBuf) -> PathBuf) {
 }
 
 // Libraries for Path manipulation
-
+#[allow(dead_code)]
 fn transform_none(path: &PathBuf) -> PathBuf {
     path.clone()
 }
 
 // Remove top N directory to the destination uri.
+//
 // Args:
 //   depth: the number of directory depth to remove.
 //
 // Returns:
 //   Transformed the path.
 #[allow(unused)]
-fn transform_moveup(depth: i32, path: &PathBuf) -> PathBuf {
+fn transform_moveup(depth: usize, path: &PathBuf) -> PathBuf {
+    if depth == 0 {
+        return path.clone();
+    }
+
+    let mut components = path.components();
+    let mut depth_adjusted = depth;
+    if ! path.starts_with("/") {
+        depth_adjusted = depth_adjusted - 1;
+    }
+    if path.starts_with("../") {
+        println!("move up transform does not support relative ../");
+        panic!();
+    }
+    components.nth(depth_adjusted);
+    components.as_path().to_path_buf()
+}
+
+// TODO: Transform each markdown into a separate directory.
+// e.g) review/item1.md --> review/item1/index.html
+#[allow(unused)]
+fn transform_seo(path: &PathBuf) -> PathBuf {
+    // TODO: Implement.
     path.clone()
 }
 
@@ -85,5 +108,28 @@ mod tests {
         tpl.render("default.html", &mut item);
 
         println!("{}", item.html);
+    }
+
+    #[test]
+    fn transform_moveup_2() {
+        let path = PathBuf::from("tests/page/review/item1.md");
+        let new_path = transform_moveup(2, &path);
+
+        assert_eq!(new_path, PathBuf::from("review/item1.md"));
+    }
+
+    #[test]
+    fn transform_moveup_3_abs() {
+        let path = PathBuf::from("/media/page/review/item.png");
+        let new_path = transform_moveup(2, &path);
+
+        assert_eq!(new_path, PathBuf::from("review/item.png"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn transform_moveup_2_upper() {
+        let path = PathBuf::from("../../review/item.md");
+        let _ = transform_moveup(2, &path);
     }
 }
